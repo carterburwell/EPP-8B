7a724168935497dae95c09e60a9c271ab8650f90
carterburwell <carterburwell@gmail.com>
2024-05-23 13:57:04 -0400

    Initial Commit

---
+++ EPP/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EPP
+//
+//  Created by Carter Burwell on 24-05-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EPP/EPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  EPPApp.swift
+//  EPP
+//
+//  Created by Carter Burwell on 24-05-23.
+//
+
+import SwiftUI
+
+@main
+struct EPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
